99f41b18f5873db6b6ea032657f382d9ce1b7c24
Prajeesh Thomas <30523998+prajeeshkolath@users.noreply.github.com>
2025-08-31 15:22:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure App Service! (deployed from github) - auto trigger -test again 🚀"
+    return "Hello from Azure App Service! (deployed from github) - auto trigger -test again-- 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
